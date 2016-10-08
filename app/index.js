12f54deefdd4e05d4d998b600a8ddf8d21f00547
cnzsb <cnzsb@foxmail.com>
2016-10-08 16:41:44 +0800

    添加jquery和moment，测试第三方库的支持

--- app/index.js
+++ app/index.js
@@ -3,7 +3,11 @@
  */
 
 require('./scss/main.scss')
+require('moment/locale/zh-cn')
 var sub = require('./sub')
+var $ = require('jquery')
+var moment = require('moment')
 var app = document.createElement('div')
 app.innerHTML = '<h1>Hello World</h1>'
 document.body.appendChild(app)
+$('body').append('<p>TIME: ' + moment().format('LLLL') + '</p>')
