f947cdfd739bb61ceb9b9bbff48839bb7b688e86
cnzsb <cnzsb@foxmail.com>
2016-10-09 14:51:37 +0800

    添加生产环境配置，分离js和第三方库并压缩

---
+++ webpack.prod.conf.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack')
+var path = require('path')
+var HtmlwebpackPlugin = require('html-webpack-plugin')
+// 定义文件夹路径
+var ROOT_PATH = path.resolve(__dirname)
+var APP_PATH = path.resolve(ROOT_PATH, 'app')
+var BUILD_PATH = path.resolve(ROOT_PATH, 'docs')
+
+module.exports = {
+    entry: {
+        app: path.resolve(APP_PATH, 'index.js'),
+        vendors: ['jquery', 'moment']
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: APP_PATH,
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass'],
+                include: APP_PATH
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url?limit=10000'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({minimize: true}),
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+        new HtmlwebpackPlugin({
+            title: 'Hello World app'
+        })
+    ]
+}
